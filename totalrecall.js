7e15db658f6fed36f3f4e3b93f8fb81cd3770a9b
git <olingageorgette1@gmail.com>
2023-10-19 15:29:17 -0400

    added finish file

---
+++ totalrecall.js
@@ -0,0 +1,21 @@
+/* 
+1-we create variables using let; const; and var
+let number = 5;
+console.log();   ---> this print the value in parenthesis on the terminal : no quotation 
+e.g 
+*/
+let firstvariable;
+// assign a value
+
+//firstvariable= "hello world"
+//
+// let number;
+// console.log("");   ----> use the quotation marks when dealing with strings: a text
+
+/* 2- How do we assign a value to a variable?
+using the signle = sign
+
+// use const for arays and objects
+
+
+*/
